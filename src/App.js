02213c91d023d70394bfd3a00283c5dd78454b57
chenyuhan <chenyuhan@corp.netease.com>
2019-10-23 14:23:10 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { connect } from '@music/mrx-react';
-import logo from './logo.svg';
 
 import './App.css';
 
 function App(props) {
   const { list } = props;
-  console.log(props)
   const [text, setText] = useState('');
+  useEffect(() => {
+    document.title = text
+  }, [text])
   function onAdd() {
-    props.addTodo()
+    props.addTodo(text)
   }
   function handleInput(e) {
-    console.log(e.target.value)
+    let value = e.target.value;
+    setText((preValue) => {
+      return value;
+    })
+  }
+  const test = function () {
+    console.log('test');
   }
   return (
     <div className="App">
       <div>
         <input type="text" onChange={handleInput}/>
-        <button>{ text }</button>
+        <button onClick={ onAdd }>onAdd</button>
       </div>
       <ul>
         {
           list.map((item) => {
             return (
-              <li key={ item.id }>{ item.title }</li>
+              <li key={ item.id } onClick={test}>{ item.title }</li>
             )
           })
         }
@@ -44,7 +51,7 @@ App.propTypes = {
 
 function mapStateToProps(store) {
   return {
-    list: store.select(list)
+    list: store.select('list')
   }
 }
 
